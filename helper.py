b71883e9e1afeebcccd508d2d41025f02716a663
PhawatL <phawat.loed@gmail.com>
2023-01-22 22:29:07 +0700

    commit main and helper

---
+++ helper.py
@@ -0,0 +1,45 @@
+e = 1e-7
+class make_fuc:
+    
+    def __init__(self,w,degree) -> None:
+        self.w = w
+        self.degree = degree
+    def fuc(self,x,*,w= None):
+        ans = 0
+        degree = self.degree
+        for i in range(len(self.w)):
+            ans +=self.w[i]*(x+e)**(degree)
+            degree -= 1
+        return ans
+    def __call__(self,x):
+        return self.fuc(x)
+    
+    def __repr__(self) -> str:
+        s = ''
+        degree = self.degree
+        for i in range(len(self.w)):
+            s += ' +' if self.w[i] >0 else ' '
+            s += f'{round(self.w[i],6)}x^{degree}'
+            degree -=1
+        
+        
+        return s
+    
+    
+class StdoutRedirector():
+    def __init__(self,widget):
+        self.widget = widget
+
+    def write(self,string):
+        self.widget.insert("end",string)
+        self.widget.see("end")
+        
+    def flush(self):
+        pass
+    
+def diff_fuc(f,g,*,n=20):
+    
+    diff = 0
+    for i in range(n):
+        diff = abs(f(i)-g(i))
+    return diff/n
---
+++ main.py
@@ -0,0 +1,134 @@
+import tkinter as tk
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from helper import make_fuc,StdoutRedirector
+import sys
+
+        
+        
+root = tk.Tk()
+canvas = tk.Canvas(root, width=800, height=600)
+canvas.pack()
+
+def draw_grid(rows, columns):
+    for i in range(rows+1):
+        canvas.create_line(0, i*50, columns*50, i*50)
+    for i in range(columns+1):
+        canvas.create_line(i*50, 0, i*50, rows*50)
+
+
+
+
+rows, columns = 12,16
+draw_grid(12, 16)
+
+coordinates = []
+
+def on_move(event):
+    x, y = event.x, event.y
+    if coordinates:
+        canvas.create_line(coordinates[-1][0], coordinates[-1][1], x, y,width=2)
+    coordinates.append((x, y))
+
+
+def set_degree(degree):
+    global upper_degree
+    upper_degree = int(degree)
+    print(f'Upper degree set to {upper_degree}')
+
+def increment_degree():
+    global upper_degree
+    if upper_degree < 20:
+        upper_degree += 1
+
+    degree_slider.set(upper_degree)
+
+def decrement_degree():
+    global upper_degree
+    if upper_degree > 1:
+        upper_degree -= 1
+
+    degree_slider.set(upper_degree)
+
+def get_coordinates():
+    global upper_degree
+    df = pd.DataFrame(coordinates,columns=['x','y'])
+    df['x']=df['x'] - 400
+    df['y']=rows-df['y'] + 300
+    
+
+    df.to_csv('coordinates.csv',index=False)
+    x = df['x'].to_numpy()
+# y = f(x)
+    y = df['y'].to_numpy()
+    print(list(zip(x,y)))
+
+    n = len(x)
+    degree_upper = upper_degree
+    degree_lower = 0
+
+
+    plt.plot(x,y,'.r')
+
+    X = x**degree_upper
+
+    for i in range(degree_upper-1,degree_lower,-1):
+        X = np.vstack(((x)**i,X))
+    else:
+        X = np.vstack((X,np.ones(n)))
+   
+    X = X.T
+    W = np.linalg.inv(X.T @ X) @ X.T @ y
+    print(W)
+    z = X @ W
+    plt.scatter(x,z,s=50)
+    my_fuc = make_fuc(W,degree_upper)
+    # print(diff_fuc(my_fuc,f,n=100))
+    print(f'predict fucntion : {my_fuc}')
+    # plt.xlim((-10,10))
+    # plt.ylim((-10,10))
+    ax = plt.gca()
+    ax.set_aspect('equal')
+    plt.show()
+def clear_canvas():
+    canvas.delete("all")
+    draw_grid(12,16)
+    canvas.create_line(0, rows*50/2, columns*50, rows*50/2, width=3) # x-axis
+    canvas.create_line(columns*50/2, 0, columns*50/2, rows*50, width=3) # y-axis
+    coordinates.clear()
+
+
+canvas.bind("<B1-Motion>", on_move)
+upper_degree = 2
+degree_slider = tk.Scale(root, from_=1, to=20, orient="horizontal", label="Degree", command=set_degree)
+degree_slider.pack()
+#create a horizontal frame to hold the buttons
+frame = tk.Frame(root)
+frame.pack()
+
+#create the buttons
+get_coords_button = tk.Button(frame, text="Predict", command=get_coordinates)
+get_coords_button.pack(side=tk.LEFT)
+clear_button = tk.Button(frame, text="Clear", command=clear_canvas)
+clear_button.pack(side=tk.LEFT)
+
+#create a text box to display the output
+output_text = tk.Text(root, width=90, height=6, wrap=tk.WORD)
+output_text.pack()
+
+#redirect the output to the text box
+sys.stdout = StdoutRedirector(output_text)
+
+#create the up and down arrow buttons
+down_arrow = tk.Button(frame, text="v", command=decrement_degree)
+down_arrow.pack(side=tk.RIGHT)
+up_arrow = tk.Button(frame, text="^", command=increment_degree)
+up_arrow.pack(side=tk.RIGHT)
+degree_slider.set(upper_degree)
+
+canvas.create_line(0, rows*50/2, columns*50, rows*50/2, width=3) # x-axis
+canvas.create_line(columns*50/2, 0, columns*50/2, rows*50, width=3) # y-axis
+
+
+root.mainloop()
